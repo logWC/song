b14eeddea98c47867e86b592c70abe6f689e768d
abc <qq1651702207@163.com>
2022-03-29 20:44:29 +0800

    添加tail尾部

---
+++ src/components/tail/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <hr />
+        <div style="text-align:center">
+        <a href="https://beian.miit.gov.cn" style="color:#939393" target="target">粤ICP备2021103978号</a>
+        <br />
+        <a
+            target="_blank"
+            href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011802000550" 
+            style="display:inline-block;height:20px;line-height:20px;">
+            <img src="../../assets/filingIcon.png" style="float:left;"/>
+            <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤公网安备 44011802000550号</p>
+        </a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'ail',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style scoped>
+    
+</style>
