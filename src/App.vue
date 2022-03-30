8f2163d64d39b6bfb3fb9179add12ab89daa2fab
abc <qq1651702207@163.com>
2022-03-30 22:02:45 +0800

    修改注释

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <!-- <div style="height:50px"></div> -->
     <router-view></router-view>
-    <!-- 为PlaySong组件留空间 -->
     <Tail />
     <PlaySong />
   </div>
 </template>
 <script>
+// 导入尾部组件
+import Tail from './components/tail/index.vue'
 // 导入下标播放组件
 import PlaySong from './components/PlaySong.vue'
-import Tail from './components/tail/index.vue'
 export default {
   components: { PlaySong, Tail },
   name: 'App',
@@ -18,10 +17,7 @@ export default {
     // 判断是否还在登录状态
     this.$api.loginStatus()
     .then(content => this.$store.dispatch('userData',content.data.profile))
-    .catch(error => console.log(error))
-  },
-  mounted(){
-    console.log("app")
+    .catch(error => console.log('未登录'))
   }
 }
 </script>
