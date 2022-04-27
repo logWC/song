7a5c5b8ff5305d21d9b352716f01134ffef95822
abc <qq1651702207@163.com>
2022-04-28 03:45:42 +0800

    优化喜欢歌曲页

--- src/routes/likes.vue
+++ src/routes/likes.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <ul @click.once="idListMe" v-if="likeList.length">
-            <li @click="play(item.id,index)" v-for="(item,index) in likeList" :key="item.id">
+        <ul @click.once="$idListMe(idList)" v-if="likeList.length">
+            <li @click="$play(item.id)" v-for="item in likeList" :key="item.id">
                 {{item.name}}
             </li>
         </ul>
@@ -17,22 +17,13 @@ export default {
         }
     },
     methods: {
-        play(id,index){
-            this.$bus.$emit('currentSong',id,index)
-        },
-        idListMe(){
-            this.$bus.$emit('musicIdList',this.idList)
-        },
         idJoin(){
-            this.idList = []
-            this.likeList.forEach(
-                val => this.idList.push(val.id)
-            )
+            this.idList = this.likeList.map(val=>val.id)
         }
     },
     computed:{
         ...mapState({
-            likeList:state => state.likeDataList
+            likeList:state=>state.likeDataList
         })
     },
     created() {
