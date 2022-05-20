6da5ac4b9163ee47ec19131b68d1998fa3cece04
abc <qq1651702207@163.com>
2022-05-20 21:16:21 +0800

    优化播放，暂停的方法

--- src/components/PlaySong.vue
+++ src/components/PlaySong.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <div class="thead" :class="{whole1:boole,whole2:!boole}" @mouseleave="out(2000)" @mouseenter="penetrate">
+        <div class="thead" :class="{whole1:boole,whole2:!boole}" @mouseleave="wearOut(2000)" @mouseenter="penetrate">
             <img :src="picUrl" alt="加载出错啦" />
-            <audio ref="audio" controls="controls" autoplay="autoplay" :src="audioSrc">
+            <audio id="audio" ref="audio" controls="controls" autoplay="autoplay" :src="audioSrc">
                 对不起，你的浏览器不支持audio标签，请升级或更换浏览器进行播放
             </audio>
             <button @click="$bus.$emit('lastSong')">上一首</button>
-            <button v-if="!suspendBoolean" @click="suspend">暂停</button>
+            <button v-if="suspendBoolean" @click="suspend">暂停</button>
             <button v-else @click="play">播放</button>
             <button @click="$bus.$emit('nextSong')">下一首</button>
         </div>
@@ -16,33 +16,34 @@
 <script>
 import PlaySongDetails from './PlaySongDetails.vue'
 export default {
-  components: { PlaySongDetails },
+    components: { PlaySongDetails },
     name:"PlaySong",
     data() {
         return {
             audioSrc:null,
             picUrl:require('@/assets/logo.png'),
-            musicObj:null,
             boole:false,
-            playsongdetailsBoole:false,
             setTime:null,
             suspendBoolean:false,
+            audioEl:null,
         }
     },
     methods: {
         /* 播放音乐 */
         music(id){
-            // 获取图片
+            // 获取歌曲图片
             this.$api.songDetail(id)
             .then(content => this.picUrl = content.data.songs[0].al.picUrl)
+            // 进行播放及获取歌词
             this.$api.song(id)
-            // 获取歌曲url
             .then(val => {
                 let url = val.data.data[0].url
                 if(url){
-                    this.audioSrc =  url + '?time=' + Math.random();
+                    // 获取歌词
                     this.$bus.$emit('obtainLyric',id)
-                    // 兼容autoplay失效的浏览器
+                    // 获取url
+                    this.audioSrc =  url + '?time=' + Math.random();
+                    // 兼容autoplay失效的浏览器（点击歌曲但无法自动播放）
                     this.$nextTick(()=>{this.play()})
                 }else{
                     // 当url为null时，播放下一首
@@ -53,16 +54,13 @@ export default {
         },
         /* 暂停 */
         suspend(){
-            if(this.$refs.audio.paused==false){
-                this.$refs.audio.pause()
-                this.suspendBoolean=true
-            }
+            !this.audioEl.paused && this.audioEl.pause()
         },
         /* 播放 */
         play(){
-            this.$refs.audio.play()
-            this.suspendBoolean=false
+            this.audioEl.paused && this.audioSrc && this.audioEl.play()
         },
+        // playsongdetailsBoole:false,
         // clickSongImg(){
         //     /* 点击进入歌词页 */
         //     this.playsongdetailsBoole = !this.playsongdetailsBoole
@@ -80,15 +78,31 @@ export default {
             clearTimeout(this.setTime)
         },
         /* 鼠标穿出元素 */
-        out(time){
+        wearOut(time){
             this.setTime = setTimeout(()=>this.boole=true,time)
+        },
+        suspendBooleanMe(){
+            console.log(111)
+            this.suspendBoolean=!this.suspendBoolean
+        },
+        // 绑定元素
+        elementMe(){
+            this.audioEl = this.$refs.audio;
+            ['play','pause'].forEach(val=>this.audioEl.addEventListener(val,this.suspendBooleanMe))
         }
     },
     created(){
         // 隐藏下标播放
-        this.out(4000)
+        this.wearOut(4000)
         // 设置全局总线事件
         this.$bus.$on('music',this.music)
+    },
+    mounted() {
+        // 设置绑定元素
+        this.elementMe()
+    },
+    beforeDestroy(){
+        ['play','pause'].forEach(val=>this.audioEl.removeEventListener(val,this.suspendBooleanMe))
     },
 }
 </script>
