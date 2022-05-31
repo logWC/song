1a2f59de7f15fc451fdcdaa0ff5d213397d6936c
abc <qq1651702207@163.com>
2022-05-31 21:52:23 +0800

    新的歌词路由

---
+++ src/routes/lyric.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name:'lyrics',
+    data() {
+        return {
+            valu:'wclog'
+        }
+    },
+    methods: {
+        /* 获取歌曲数据 */
+        getSongData(val){
+            console.log(val)
+        }
+    },
+    created() {
+        // 接收歌曲数据
+        this.$bus.$on('getSongData',this.getSongData)
+        // 请求歌曲数据
+        this.$bus.$emit('sendSongData')
+    },
+    render(h) {
+        return h('h1',{},['this.$route.query.audioEl.currentTime'])
+    },
+}
+</script>
+<style scoped>
+
+</style>
