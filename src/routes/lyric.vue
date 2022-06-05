0c43263a9d0df03139aeaeb7d98bc7d5bca18f6c
abc <qq1651702207@163.com>
2022-06-06 02:35:57 +0800

    新的歌词路由

--- src/routes/lyric.vue
+++ src/routes/lyric.vue
@@ -1,25 +1,41 @@
 <script>
+import { mapState } from "vuex";
 export default {
     name:'lyrics',
     data() {
         return {
-            valu:'wclog'
+            lyric:[]
         }
     },
     methods: {
         /* 获取歌曲数据 */
-        getSongData(val){
-            console.log(val)
-        }
+        getAudioTab(val){
+            console.log(val);
+            console.log(this.valu)
+        },
+    },
+    computed:{
+        ...mapState({
+            currentTimeList:state=>state.song.currentTimeList,
+            currentContentList:state=>state.song.currentContentList,
+            map:state=>state.song.map,
+            orderStr:state=>state.song.orderArr[state.song.orderNum]
+        })
     },
     created() {
         // 接收歌曲数据
-        this.$bus.$on('getSongData',this.getSongData)
+        // this.$bus.$on('getAudioTab',this.getAudioTab)
         // 请求歌曲数据
-        this.$bus.$emit('sendSongData')
+        // this.$bus.$emit('sendAudioTab')
     },
     render(h) {
-        return h('h1',{},['this.$route.query.audioEl.currentTime'])
+        this.lyric = h('div',this.currentContentList.map(val => h('p',val)))
+        const lastButton = h('button',{
+            on:{
+                click: () => this.$store.dispatch('song/lastMe')
+            }
+        },'上一首')
+        return h('div',{},[this.lyric,lastButton])
     },
 }
 </script>
