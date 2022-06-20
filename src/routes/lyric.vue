82d7d3bcdcbd5f4b676dd95d8dea04945833168f
abc <qq1651702207@163.com>
2022-06-21 01:08:50 +0800

    lyric添加歌曲图片为背景毛玻璃

--- src/routes/lyric.vue
+++ src/routes/lyric.vue
@@ -136,8 +136,9 @@ export default {
     height: inherit;
     position: absolute;
     top: 0;left: 0;right: 0;bottom: 0;
-    background-color: rgba(255, 255, 255, 0.5);
-    backdrop-filter: blur(30px);
+    background-color: rgba(128, 128, 128, 0.5);
+    backdrop-filter: blur(20px);
+
 }
 .headr{
     width: inherit;
@@ -154,7 +155,7 @@ export default {
 .pic{
     flex-basis: 50%;
     height: inherit;
-    background-color: yellowgreen;
+    /* background-color: yellowgreen; */
 }
 .lyric{
     flex-basis: 50%;
