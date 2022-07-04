82c3f8a5da36c95d7fa6577925c6752222074672
abc <qq1651702207@163.com>
2022-07-05 00:06:54 +0800

    增加like组件（公共组件）

---
+++ src/components/like.vue
@@ -0,0 +1,57 @@
+<template>
+    <svg @click="likeMe" class="icon" aria-hidden="true">
+        <use :xlink:href="icon"></use>
+    </svg>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+    name:'likeIcon',
+    props:{id:Number},
+    data() {
+        return {
+            icon:'#icon-aixinD'
+        }
+    },
+    methods:{
+        likeMe(){
+            this.$api.like(this.id,this.icon=='#icon-aixinD')
+            .then(val=>{
+                this.$store.dispatch('profiles/obtainLikeList');
+                this.getSee()
+            })
+            .catch(error=>{
+                error.toString().includes(500)
+                ?alert('没有版权,喜欢失败')
+                :alert('未知错误,喜欢失败')
+            })
+        },
+        getSee(){
+            if(this.id){
+                this.icon = this.likeIdList.includes(this.id)
+                ?'#icon-aixinshoucang'
+                :'#icon-aixinD';
+            }
+        }
+    },
+    watch:{
+        id:{
+            handler(val){
+                val && this.getSee();
+            },
+            immediate:true
+        },
+        likeIdList:{
+            handler(val){
+                val && this.getSee();
+            },
+            immediate:true
+        }
+    },
+    computed:{
+        ...mapState({
+            likeIdList:state=>state.profiles.likeIdList
+        })
+    }
+}
+</script>
