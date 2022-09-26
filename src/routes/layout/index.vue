5166e5ca2ab6d7e6a5a658e78f8b670c4465f3b4
abc <qq1651702207@163.com>
2022-09-26 19:48:15 +0800

    清除无效代码

--- src/routes/layout/index.vue
+++ src/routes/layout/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="rootDiv">
         <Nav />
-        <keep-alive include="Home,Mine">
+        <keep-alive>
             <router-view></router-view>
         </keep-alive>
     </div>
